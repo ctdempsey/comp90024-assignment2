e1abf73a288af6fefff766351acc79646a23de87
Cameron Dempsey <cdempsey1@student.unimelb.edu.au>
2020-05-26 13:41:12 +1000

    Update tweet_harvester.py with Twitter API details

--- tweet_harvester/tweet_harvester.py
+++ tweet_harvester/tweet_harvester.py
@@ -1,12 +1,9 @@
+import os
 import sys
 
 # import pandas as pd
 import tweepy
 import couchdb
-
-# TODO: Handle couchdb.http.Unauthorized exception
-couch = couchdb.Server()
-tweetdb = couch['tweets']
 
 
 # Request tokens and keys for authentication
@@ -18,7 +15,14 @@ def get_authentication(sys_args):
     return api_key, api_secret_key, access_token, access_token_secret
 
 
-def search_tags(api_key, api_secret_key, access_token, access_token_secret, hashtag):
+def get_couchdb_details(sys_args):
+    cdb_user = sys_args[0]
+    cdb_password = sys_args[1]
+    cdb_port = sys_args[2]
+    return cdb_user, cdb_password, cdb_port
+
+
+def search_tags(api_key, api_secret_key, access_token, access_token_secret, hashtag, tweetdb):
     # Handles Twitter authentication
     access = tweepy.OAuthHandler(api_key, api_secret_key)
     access.set_access_token(access_token, access_token_secret)
@@ -26,39 +30,49 @@ def search_tags(api_key, api_secret_key, access_token, access_token_secret, hash
     # start api with tweepy
     api = tweepy.API(access, wait_on_rate_limit=True)
 
-    # create df to write to
-    # df = pd.DataFrame(columns=['id', 'timestamp', 'location', 'tweet_text', 'username', 'all_hashtags', 'followers_count'])
 
-    # extract tweets with the relevant tag, write to the file
-    for tweet in tweepy.Cursor(api.search, q=hashtag + ' -filter:retweets',
-                               lang="en", tweet_mode='extended').items(100):
-        if tweet.coordinates:
-            tweetdb[tweet.id] = {'created_at': tweet.created_at,
-                                 'coordinates': tweet.coordinates,
-                                 'full_text': tweet.full_text.replace('\n', ' ').encode('utf-8'),
-                                 'user': tweet.user.screen_name.encode('utf-8'),
-                                 'hashtags': [e['text'] for e in tweet._json['entities']['hashtags']],
-                                 'user_followers': tweet.user.followers_count}
-        # if tweet.coordinates:
-        #     df.loc[len(df)] = [tweet.id,
-        #                        tweet.created_at,
-        #                        tweet.coordinates,
-        #                        tweet.full_text.replace('\n', ' ').encode('utf-8'),
-        #                        tweet.user.screen_name.encode('utf-8'),
-        #                        [e['text'] for e in tweet._json['entities']['hashtags']],
-        #                        tweet.user.followers_count]
+    # extract tweets with the relevant tag, write to database
+    for tweet in tweepy.Cursor(api.search, q=hashtag + ' -filter:retweets -has:geo', lang="en", tweet_mode='extended',
+                               geocode='-28.04234848,133.49058772,2100km').items(1000):
+        try:
+            tweet_data = {'_id': tweet.id_str,
+                        'created_at': tweet.created_at.isoformat(),
+                        'coordinates': tweet.coordinates,
+                        'place': tweet.place,
+                        'full_text': tweet.full_text,
+                        'user_id': tweet.user.id_str,
+                        'user_screen_name': tweet.user.screen_name,
+                        'hashtags': [e['text'] for e in tweet._json['entities']['hashtags']],
+                        'user_followers': tweet.user.followers_count}
+            if tweet.place:
+                place_data = {'id': tweet.place.id,
+                              'place_type': tweet.place.place_type,
+                              'name': tweet.place.name,
+                              'full_name': tweet.place.full_name,
+                              'country_code': tweet.place.country_code,
+                              'country': tweet.place.country,
+                              'contained_within': tweet.place.contained_within,
+                              'bounding_box': {'type':
 
-    # open the spreadsheet we will write to
-    # f = open(hashtag + ".json", 'w')
-    # df.to_json(f, orient='records')  # lines=True)
-    # f.close()
+                              }}
+                tweet_data['place'] = place_data
+            tweetdb.save(tweet_data)
+        except TypeError:
+            print(tweet.place)
+        except couchdb.http.ResourceConflict:
+            pass
 
+    tweetdb.commit()
 
 def main():
-    sys.stderr = open('tweet_harvester.err', 'a')
+    # sys.stderr = open(os.path.expanduser('~/tweet_harvester.err'), 'a')
     hashtag = sys.argv[1]
-    api_key, api_secret_key, access_token, access_token_secret = get_authentication(sys.argv[2:])
-    search_tags(api_key, api_secret_key, access_token, access_token_secret, hashtag)
+    api_key, api_secret_key, access_token, access_token_secret = get_authentication(sys.argv[2:6])
+    cdb_user, cdb_password, cdb_port = get_couchdb_details(sys.argv[6:])
+    cdb_url = 'http://' + cdb_user + ':' + cdb_password + '@localhost:' + cdb_port + '/'
+    couch = couchdb.Server(cdb_url)
+    tweetdb = couch['tweets']
+    search_tags(api_key, api_secret_key, access_token, access_token_secret, hashtag, tweetdb)
 
 
 if __name__ == '__main__':
